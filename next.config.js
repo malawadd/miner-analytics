ffea22b7fbc11bdd6f62d19b219bd57b95be3260
malawadd <50057882+malawadd@users.noreply.github.com>
2023-02-01 13:15:45 +0300

    🌋 spheron image error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  env: {
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+  },
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+};
