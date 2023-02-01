cc58ebaa73f8898067fd3cb1f26f4fb61782b79b
malawadd <50057882+malawadd@users.noreply.github.com>
2023-02-01 16:28:09 +0300

    📱 mobile friendly

--- components/HeaderNavbar/index.js
+++ components/HeaderNavbar/index.js
@@ -134,6 +134,38 @@ import {
             />
           </Group>
             </Header>
+
+            <Drawer
+          opened={drawerOpened}
+          onClose={closeDrawer}
+          size="100%"
+          padding="md"
+          title="Navigation"
+          className={classes.hiddenDesktop}
+          zIndex={1000000}
+        >
+          <ScrollArea sx={{ height: "calc(100vh - 60px)" }} mx="-md">
+            <Divider
+              my="sm"
+              color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"}
+            />
+
+            <Link component="a" href="/" className={classes.link}>
+              Home
+            </Link>
+            <Link component="a" href="/miners" className={classes.link}>
+              miners
+            </Link>
+            <Divider
+              my="sm"
+              color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"}
+            />
+
+            <Group position="center" grow pb="xl" px="md">
+              <ColorModeButton />
+            </Group>
+          </ScrollArea>
+        </Drawer>
         </Box>
         </>
     )
