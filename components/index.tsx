6cebc1ab03fbe829ab9a728959a405fe8630d908
malawadd <50057882+malawadd@users.noreply.github.com>
2023-02-01 18:50:23 +0300

    🦎 coingecko api

--- components/index.tsx
+++ components/index.tsx
@@ -2,3 +2,4 @@ export { default as HeaderNavbar } from "./HeaderNavbar";
 export { default as Footer } from "./Footer";
 export { default as SideNavbarMiners } from "./SideNavbarMiners";
 export { default as Miners } from "./miners";
+export { default as LoaderComp } from "./LoaderComp";
--- components/miners/index.js
+++ components/miners/index.js
@@ -1,7 +1,65 @@
+import React from "react";
+import { Center, Notification, Text, SimpleGrid } from "@mantine/core";
+import { Flex } from "@chakra-ui/react";
+import { IconX } from "@tabler/icons";
+import { useQuery } from "react-query";
+import moment from "moment";
+import LoaderComp from "../LoaderComp";
+
+
+
 export default function MinersOverview() {
+  // used React-Query to fetch coingecko API
+  const { data, error, isFetching } = useQuery(["Price"], async () => {
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/filecoin/market_chart?vs_currency=usd&days=183&interval=daily"
+    );
+    return res.json();
+  });
+
+  console.log("data is ", data);
+
+  // Chart data for filecoin market_caps
+  const marketCap = data?.market_caps.map((item) => ({
+    x: moment(item[0]).format("MMM Do"),
+    MarketCap: item[1],
+  }));
+
+  // Chart data for filecoin price
+  const prices = data?.prices.map((item) => ({
+    x: moment(item[0]).format("MMM Do"),
+    Price: item[1],
+  }));
+
+  if (isFetching) return <LoaderComp />;
+
+  if (error)
     return (
-        <>
-         <div> hey</div>
-        </>
-      );
+      <Center
+        style={{
+          width: "100%",
+          height: "20%",
+
+          left: "0px",
+          top: "0px",
+        }}
+      >
+        <Notification icon={<IconX size={18} />} color="red">
+          Error! Failed to Fetch filecoin Chart API
+        </Notification>
+      </Center>
+    );
+
+  return (
+    <>
+      <Text c="dimmed" fz="xl" tt="uppercase">
+        Miners Overview
+      </Text>
+      <Flex justifyContent="space-evenly">
+        <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+
+        </SimpleGrid>
+      </Flex>
+    </>
+  );
 }
