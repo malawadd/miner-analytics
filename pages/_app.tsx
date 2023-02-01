ef8588792b5940c394b05cf8423df7e63ba013ab
malawadd <50057882+malawadd@users.noreply.github.com>
2023-02-01 14:17:18 +0300

    🏔️ front page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,58 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from "@mantine/core";
+import { useState } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// import react-query
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// Create a client for react-query
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export default function App(props: AppProps) {
+  // Defining Color Mode
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("dark");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            colorScheme,
+            /** mantine theme override code here */
+            breakpoints: {
+              xs: 500,
+              sm: 800,
+              md: 1000,
+              lg: 1200,
+              xl: 1400,
+            },
+            loader: "bars",
+          }}
+        >
+          <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+          </QueryClientProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
 }
